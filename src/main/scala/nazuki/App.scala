629ed6c3eba2c8dd7f9c6dc43c2be4a0ccd9ba5c
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-12-23 00:25:02 +0900

    Update App

--- src/main/scala/nazuki/App.scala
+++ src/main/scala/nazuki/App.scala
@@ -2,6 +2,8 @@ package nazuki
 
 import slinky.core._
 import slinky.core.annotations.react
+import slinky.core.facade.Fragment
+import slinky.core.facade.Hooks._
 import slinky.web.html._
 
 import scala.scalajs.js
@@ -11,30 +13,12 @@ import scala.scalajs.js.annotation.JSImport
 @js.native
 object AppCSS extends js.Object
 
-@JSImport("resources/logo.svg", JSImport.Default)
-@js.native
-object ReactLogo extends js.Object
-
-@react class App extends StatelessComponent {
+@react object App {
   type Props = Unit
 
-  private val css = AppCSS
-
-  def render() = {
-    div(className := "App")(
-      header(className := "App-header")(
-        img(
-          src := ReactLogo.asInstanceOf[String],
-          className := "App-logo",
-          alt := "logo"
-        ),
-        h1(className := "App-title")("Welcome to React (with Scala.js!)")
-      ),
-      p(className := "App-intro")(
-        "To get started, edit ",
-        code("App.scala"),
-        " and save to reload."
-      )
+  val component = FunctionalComponent[Props] { props =>
+    Fragment(
+      h1(s"Hello")
     )
   }
 }
